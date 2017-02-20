6dc49803b4f3223141d82dd7194dd7456f66ffb4
David Dinh <ddinh1@uci.edu>
2017-02-19 17:55:09 -0800

    Add initial HTML / CSS for club profile page.

--- public/controllers/clubprofile.controller.js
+++ public/controllers/clubprofile.controller.js
@@ -1,17 +1,6 @@
 'use strict';
 
 angular.module('ClubTree')
-    .config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('clubprofile', {
-                    url: "/clubprofile/:id",
-                    templateUrl: '../views/clubprofile.view.html',
-                    controller: 'ClubProfile'
-                });
-        }])
     .controller('ClubProfile', ['$scope', function ($scope) {
         $scope.viewingClubProfile;
         console.log('you are inside the controller');
