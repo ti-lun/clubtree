d98dec853f8e1eed6e7e15649e23df565f3425d8
David Dinh <ddinh1@uci.edu>
2017-02-26 17:55:07 -0800

    Promisify the club profile service. Add a 1-second delay to mimick network latency. I also used $scope.apply() in the controller because I don't know how else to refresh the digest cycle.

--- public/controllers/clubprofile.controller.js
+++ public/controllers/clubprofile.controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('ClubTree').controller('ClubProfile', [
-    '$scope',
-    'ClubAPI',
-    function ($scope, ClubAPI) {
+    '$scope', '$stateParams', 'ClubAPI',
+    function ($scope, $stateParams, ClubAPI) {
         console.log('you are inside the controller 2');
-        $scope.viewingClubProfile = ClubAPI.get();
+        $scope.viewingClubProfile =  ClubAPI.get($stateParams.id).then(function (response) {
+            $scope.viewingClubProfile = response;
+            $scope.$apply();
+        });
     }]);
